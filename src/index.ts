49269848dda89c88374dd4c954374efd3cb2cebd
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-02-24 19:51:01 +0800

    :construction: WIP - added utcTimeSync and utcTime

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,74 @@
 // @ts-check
 
-export async function greeting(
-  name: string = 'World'
-) {
-  try {
-    return await greetingSync(name);
-  } catch (e) {
-    throw e;
+export declare interface UTCTimeOffset {
+  hour?: number;
+  minute?: number;
+  second?: number;
+  millisecond?: number;
+}
+export declare interface UTCTimeOpts {
+  startDatetime?: string | number | Date;
+  offset?: UTCTimeOffset;
+}
+
+function isValidDatetime(datetime: any) {
+  return !/^invalid date/i.test(`${new Date(datetime)}`);
+}
+
+function isNotANumber(value: any) {
+  return value != null && Number.isNaN(+value);
+}
+
+export async function utcTimeSync({
+  startDatetime,
+  offset,
+}: UTCTimeOpts) {
+  const isNullishDatetime = startDatetime == null;
+
+  if (!isNullishDatetime && !isValidDatetime) {
+    throw new TypeError('Param opts[startDatetime] is not a valid datetime');
   }
+
+  const {
+    hour = 0,
+    minute = 0,
+    second = 0,
+    millisecond = 0,
+  } = offset || {} as UTCTimeOffset;
+
+  if (isNotANumber(hour)) {
+    throw new TypeError('Param opts[offset][hour] is not a number');
+  }
+
+  if (isNotANumber(minute)) {
+    throw new TypeError('Param opts[offset][minute] is no a number');
+  }
+
+  if (isNotANumber(second)) {
+    throw new TypeError('Param opts[offset][second] is not a number');
+  }
+
+  if (isNotANumber(millisecond)) {
+    throw new TypeError('Param opts[offset][millisecond] is not a number');
+  }
+
+  const newDatetime = isNullishDatetime
+    ? new Date()
+    : new Date(startDatetime as string);
+
+  return new Date(Date.UTC(
+    newDatetime.getUTCFullYear(),
+    newDatetime.getUTCMonth(),
+    newDatetime.getUTCDate(),
+    newDatetime.getUTCHours() + hour,
+    newDatetime.getUTCMinutes() + minute,
+    newDatetime.getUTCSeconds() + second,
+    newDatetime.getUTCMilliseconds() + millisecond
+  ));
 }
 
-export function greetingSync(
-  name: string = 'World'
-) {
-  return `Hello, ${name}!`;
+export async function utcTime(opts: UTCTimeOpts) {
+  return utcTimeSync(opts);
 }
 
-export default greeting;
+export default utcTime;
